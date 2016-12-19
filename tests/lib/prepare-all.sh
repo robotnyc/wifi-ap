2d059f12aa21de1c12364ce5d50bd5d83a1184c5
Simon Fels <simon.fels@canonical.com>
2016-12-19 11:40:50 +0100

    Load mac80211_hwsim driver a bit earlier before the first test runs
    The driver was loaded after the prepare script checked for the existence
    of a locally build .snap file and if present it skipped loading the driver
    which caused the first test to always fail.

--- tests/lib/prepare-all.sh
+++ tests/lib/prepare-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Simulate two WiFi radio network interfaces
+modprobe mac80211_hwsim radios=2
+
 # We don't have to build a snap when we should use one from a
 # channel
 if [ -n "$SNAP_CHANNEL" ] ; then
@@ -11,9 +14,6 @@ fi
 if [ -e /home/wifi-ap/wifi-ap_*_amd64.snap ] ; then
 	exit 0
 fi
-
-# Simulate two WiFi radio network interfaces
-modprobe mac80211_hwsim radios=2
 
 # Setup classic snap and build the wifi-ap snap in there
 snap install --devmode --beta classic
