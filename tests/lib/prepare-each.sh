32d9f117edcb8e43d03243dce33a1f7adfc5d0d8
Simon Fels <simon.fels@canonical.com>
2017-03-17 13:19:31 +0100

    Powercycle wifi interfaces before we start each test case

--- tests/lib/prepare-each.sh
+++ tests/lib/prepare-each.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 . $TESTSLIB/utilities.sh
+
+# Powercycle both interface to get them back into a sane state before
+# we install the wifi-ap snap
+for d in wlan0 wlan1 ; do
+    phy=$(iw dev $d info | awk '/wiphy/{print $2}')
+    snap install --devmode wireless-tools
+    /snap/bin/wireless-tools.rfkill block $phy
+    /snap/bin/wireless-tools.rfkill unblock $phy
+done
+
 install_snap_under_test
