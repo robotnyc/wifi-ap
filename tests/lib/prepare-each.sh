3d0c86ca44e4361e6ac7709e04732e099a977285
Simon Fels <simon.fels@canonical.com>
2017-03-17 17:11:07 +0100

    Give hostapd a bit more time to come up and install wireless-tools correctly

--- tests/lib/prepare-each.sh
+++ tests/lib/prepare-each.sh
@@ -3,11 +3,14 @@
 
 # Powercycle both interface to get them back into a sane state before
 # we install the wifi-ap snap
+snap install --devmode wireless-tools
 for d in wlan0 wlan1 ; do
     phy=$(iw dev $d info | awk '/wiphy/{print $2}')
-    snap install --devmode wireless-tools
     /snap/bin/wireless-tools.rfkill block $phy
     /snap/bin/wireless-tools.rfkill unblock $phy
 done
+snap remove wireless-tools
 
 install_snap_under_test
+# Give wifi-ap a bit time to settle down to avoid clashed
+sleep 5
